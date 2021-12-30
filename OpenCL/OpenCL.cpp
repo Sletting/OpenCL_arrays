1ef0cb8a852ff4edbef962c341bc6628253eec06
Sletting <jonasplaying@outlook.com>
2021-12-30 23:32:01 +0100

    Hello World

--- OpenCL/OpenCL.cpp
+++ OpenCL/OpenCL.cpp
@@ -1,20 +1,52 @@
-// OpenCL.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_ENABLE_PROGRAM_CONSTRUCTION_FROM_ARRAY_COMPATIBILITY
+#include <CL/cl2.hpp>
+#include <CL/opencl.hpp>
+#include <fstream>
 #include <iostream>
+
+void helloWorld()
+{
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    _ASSERT(platforms.size() > 0);
+
+    auto platform = platforms.front();
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+
+    _ASSERT(devices.size() > 0);
+
+    auto device = devices.front();
+    auto vendor = device.getInfo<CL_DEVICE_VENDOR>();
+    auto version = device.getInfo<CL_DEVICE_VERSION>();
+
+    std::ifstream kernelfile("kernelfile.cl");
+    std::string src(std::istreambuf_iterator<char>(kernelfile), (std::istreambuf_iterator<char>()));
+
+    cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
+
+    cl::Context context(device);
+    cl::Program program(context, sources);
+
+    auto err = program.build("-cl-std=CL1.2");
+
+    char buf[16];
+    cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
+    cl::Kernel kernel(program, "kernelfile", &err);
+    kernel.setArg(0, memBuf);
+
+    cl::CommandQueue queue(context, device);
+    queue.enqueueTask(kernel);
+    queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
+
+    std::cout << buf;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    helloWorld();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
