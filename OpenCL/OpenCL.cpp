3d91421df82a626ac4bab818a7da0f17c9d82d0b
Sletting <jonasplaying@outlook.com>
2022-01-02 11:12:08 +0100

    ProcessArray

--- OpenCL/OpenCL.cpp
+++ OpenCL/OpenCL.cpp
@@ -7,36 +7,42 @@
 #include <fstream>
 #include <iostream>
 
-void helloWorld()
+cl::Program CreateProgram(const std::string& file) 
 {
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-
     _ASSERT(platforms.size() > 0);
 
     auto platform = platforms.front();
     std::vector<cl::Device> devices;
     platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
-
     _ASSERT(devices.size() > 0);
 
     auto device = devices.front();
     auto vendor = device.getInfo<CL_DEVICE_VENDOR>();
     auto version = device.getInfo<CL_DEVICE_VERSION>();
 
-    std::ifstream kernelfile("kernelfile.cl");
+    std::ifstream kernelfile(file);
     std::string src(std::istreambuf_iterator<char>(kernelfile), (std::istreambuf_iterator<char>()));
 
     cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
-
     cl::Context context(device);
     cl::Program program(context, sources);
-
     auto err = program.build("-cl-std=CL1.2");
+
+    return program;
+}
+
+void helloWorld()
+{
+    auto program = CreateProgram("kernelfile.cl");
+    auto context = program.getInfo<CL_PROGRAM_CONTEXT>();
+    auto devices = context.getInfo<CL_CONTEXT_DEVICES>();
+    auto& device = devices.front();
 
     char buf[16];
     cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
-    cl::Kernel kernel(program, "kernelfile", &err);
+    cl::Kernel kernel(program, "kernelfile");
     kernel.setArg(0, memBuf);
 
     cl::CommandQueue queue(context, device);
@@ -46,7 +52,37 @@ void helloWorld()
     std::cout << buf;
 }
 
+void ProcessArray()
+{
+    auto program = CreateProgram("ProcessArray.cl");
+    auto context = program.getInfo<CL_PROGRAM_CONTEXT>();
+    auto devices = context.getInfo<CL_CONTEXT_DEVICES>();
+    auto& device = devices.front();
+
+    std::vector<int> vec(1024);
+    // Use CPU to fill.
+    //std::fill(vec.begin(), vec.end(), 1);
+
+    cl::Buffer inBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, sizeof(int) * vec.size(), vec.data());
+    cl::Buffer outBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(int) * vec.size());
+    cl::Kernel kernel(program, "ProcessArray");
+    kernel.setArg(0, inBuf);
+    kernel.setArg(1, outBuf);
+    cl::CommandQueue queue(context, device);
+  
+    // Use GPU to fill buffer. Skip first 10 and fill rest with 3.
+    queue.enqueueFillBuffer(inBuf, 3, sizeof(int) * 10, sizeof(int) * (vec.size() - 10));
+    
+    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(vec.size()));
+    queue.enqueueReadBuffer(outBuf, CL_FALSE, 0, sizeof(int) * vec.size(), vec.data());
+    
+    cl::finish();
+
+    std::cin.get();
+}
+
+
 int main()
 {
-    helloWorld();
+    ProcessArray();
 }
